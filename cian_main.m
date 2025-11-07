dddf25fc70d6c2828c285c31d37058ab8d83ceda
Cianzoid28 <157330096+Cianzoid28@users.noreply.github.com>
2025-11-07 16:42:02 -0500

    Progress on accel and box rate

--- cian_main.m
+++ cian_main.m
@@ -7,8 +7,14 @@ theta = pi/6;
 Plist_box = [1, -1, -1, 1; 1, 1, -1, -1];
 
 
-Plist_world = compute_rbt(x,y,theta,Plist_box)
+Plist_world = compute_rbt(x,y,theta,Plist_box);
 
 plot(Plist_world(1,:), Plist_world(2,:))
+
+k = 1;
+l0 = 1;
+PA = [0,0];
+PB = [0,2];
+
 grid on
 axis equal
