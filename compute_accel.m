f526234d1973ea5c5e7be78bd1448590835950f6
Cianzoid28 <157330096+Cianzoid28@users.noreply.github.com>
2025-11-16 21:42:15 -0500

    Compute accel works

--- compute_accel.m
+++ compute_accel.m
@@ -15,48 +15,56 @@
 %   points for the spring (in the world frame)
 %box_params.P_box: 2 x n list of mounting points
 %    for the spring (in the box frame)
-%
+
 %OUTPUTS
 %ax: x acceleration of the box
 %ay: y acceleration of the box
 %atheta: angular acceleration of the box
-function [ax,ay,atheta] = compute_accel(x,y,theta,box_params)
+function [ax, ay, atheta] = compute_accel(x, y, theta, box_params)
     m = box_params.m;
     I = box_params.I;
     g = box_params.g;
     k_list = box_params.k_list;
     l0_list = box_params.l0_list;
-    P_world = box_params.P_world;
-    P_box = box_params.P_box
-    num_springs = length(k_list);
-    a_linear = [0; -g];
-    atheta = 0;
+    
+    P_box = box_params.P_box;        % points on box (box frame)
+    P_world_fixed = box_params.P_world;  % fixed environment anchors
+    
+    % transform box points into world frame
+    P_box_world = compute_rbt(x, y, theta, P_box);
+    P_list_world = compute_rbt(x, y, theta, P_box);
 
-    %initialize totals
+    num_springs = length(k_list);
+
     Fx_total = 0;
     Fy_total = 0;
     torque_total = 0;
 
-    Plist_world = compute_rbt(x,y,theta,P_box)
-
     for spring = 1:num_springs
-        Pw = P_world(:,spring); %fixed point in environment
-        Pb = Plist_world(:,spring); %fixed point on box
-        l = norm(Pb-Pw); %current spring length
-        e_s = (Pb-Pw)/1; %unit vector from Pw to Pb
-        F_i = -k_list(num_springs) * (l - l0_list(num_springs)) * e_s;
-        Fx_total = Fx_total + F_i(1); %add the sum of all forces acting in the x direction
-        Fy_total = Fy_total + F_i(2); %add the sum of all forces acting in the y direction
+        Pw = P_world_fixed(:, spring);   % fixed world anchor
+        Pb = P_list_world(:, spring);     % box point in world frame
+        
+        d = Pb - Pw; 
+        l = norm(d);  
+        e_s = d / l;                     % unit vector from Pw → Pb
+        
+        % spring force magnitude
+        F_i = k_list(spring) * (l - l0_list(spring)) * e_s;
 
-        r_i = Pb - [x; y]; %position vector from centroid
-        torque_i = r_i(1)*F_i(2) - r_i(2)*F_i(1); %2d cross product
-        torque_total = torque_total + torque_i; %add sum of all torque
+        Fx_total = Fx_total + F_i(1);
+        Fy_total = Fy_total + F_i(2);
+
+        % torque about COM
+        r_i = Pb - [x; y];
+        torque_i = r_i(1)*F_i(2) - r_i(2)*F_i(1);
+        torque_total = torque_total + torque_i;
     end
 
-    Fy_total = Fy_total - m * g; %add gravity
+    % add gravity
+    Fy_total = Fy_total - m * g;
 
-    %Compute acceleration
-    ax = Fx_total/m;
-    ay = Fy_total/m;
-    atheta = torque_total/I;
+    % linear and angular acceleration
+    ax = Fx_total / m;
+    ay = Fy_total / m;
+    atheta = torque_total / I;
 end
